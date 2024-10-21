a4dfbb25c45191f1ac5d8d015095c00e9974ed84
wxx9248 <wxx9248@wxx9248.top>
2024-10-20 20:25:57 -0700

    Add 404 Handling (#5)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <v-app>
-    <main>
-      <router-view />
-    </main>
+    <router-view />
   </v-app>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+(() => {
+  const { searchParams } = new URL(location.href);
+  if (!searchParams.size) {
+    return;
+  }
+
+  const path = searchParams.get("path");
+  if (!path) {
+    return;
+  }
+
+  router.replace(path);
+})();
+</script>
---
+++ src/view/NotFoundView.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container
+    id="container"
+    class="d-flex flex-column align-center justify-center"
+  >
+    <h1 id="title">404</h1>
+    <h2 id="subtitle">Page Not Found</h2>
+    <v-btn
+      id="button-homepage"
+      append-icon="mdi-home"
+      size="large"
+      @click="onButtonHomepagePress"
+      >Homepage</v-btn
+    >
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+function onButtonHomepagePress() {
+  router.push("/");
+}
+</script>
+
+<style scoped>
+#container {
+  width: 100%;
+  height: 100%;
+  user-select: none;
+}
+
+#title {
+  font-size: 10rem;
+}
+
+#subtitle {
+  font-size: 1.5rem;
+}
+
+#button-homepage {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+</style>
