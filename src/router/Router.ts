f65b2772b4715e0cb2c188567a737587ea3fa94e
wxx9248 <wxx9248@wxx9248.top>
2024-10-20 20:23:39 -0700

    Add 404 Handling

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -1,9 +1,11 @@
 import IndexView from "@/view/IndexView.vue";
+import NotFoundView from "@/view/NotFoundView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { path: "/", component: IndexView },
-  { path: "/:pathMatch(.*\\/)", component: IndexView }
+  { path: "/:pathMatch(.*\\/)", component: IndexView },
+  { path: "/:pathMatch(.*)", component: NotFoundView }
 ];
 
 export const router = createRouter({
